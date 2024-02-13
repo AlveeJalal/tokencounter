f1e75772601e301d9305a441d0c5be2c95be30ee
Alvee <alveej520@gmail.com>
2024-02-13 12:12:43 -0500

    Finished token price calculation method and tested all methods

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -6,3 +6,11 @@ def calculateTokenCount(num_of_characters: int):
     if num_of_characters % 4 > 0:
         tokenCount +=1
     return tokenCount
+
+def calculateTokenPriceInput(tokenCount:int):
+   return tokenCount/100000
+
+def calculateTokenPriceOutput(tokenCount:int):
+   return tokenCount/300000
+
+
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,4 +1,4 @@
-from calculator import characterCounter, calculateTokenCount
+from calculator import characterCounter, calculateTokenCount, calculateTokenPriceInput, calculateTokenPriceOutput
 from faker import Faker
 
 def test_character_count():
@@ -7,4 +7,11 @@ def test_character_count():
     assert characterCounter(text) == 5
 
 def test_token_count():
-    assert calculateTokenCount(5) == 2
+    assert calculateTokenCount(1000) == 250
+
+def test_token_price():
+    faker = Faker()
+    text  = faker.texts(nb_texts=4000)
+    characterCount = characterCounter(text)
+    tokenCount = calculateTokenCount(characterCount)
+    assert calculateTokenPriceInput(tokenCount) == 0.01
